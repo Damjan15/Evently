767622fd2797fe251e80fe90f21c678474e4f5b7
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-14 11:20:11 +0100

    feat: added location input

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -20,6 +20,7 @@ import { eventDefaultValues } from "@/constants";
 import Dropdown from "./Dropdown";
 import { FileUploader } from "./FileUploader";
 import { useState } from "react";
+import Image from "next/image";
 
 type EventFormProps = {
   userId: string;
@@ -108,6 +109,28 @@ const EventForm = ({ userId, type }: EventFormProps) => {
               <FormItem className="w-full">
                 <FormControl className="h-72">
                   <FileUploader onFieldChange={field.onChange} imageUrl={field.value} setFiles={setFiles} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+        <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[55px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image src="/assets/icons/location-grey.svg" alt="calendar" width={24} height={24} />
+                    <Input
+                    placeholder="Event title"
+                    {...field}
+                    className="input-field"
+                  />
+                  </div>
                 </FormControl>
                 <FormMessage />
               </FormItem>
