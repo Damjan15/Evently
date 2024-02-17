ae1586756a7e7515fd0480aab39cc3b459ad0ce5
Damjan15 <stamenovdamjan5@gmail.com>
2024-02-17 13:39:54 +0100

    fix: removed section from Profile page

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -11,31 +11,6 @@ const Profile = async () => {
     const organizedEvents = await getEventsByUser({ userId, page: 1 });
   return (
     <>
-      {/* My Tickets Section */}
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <div className="wrapper flex items-center justify-center sm:justify-between">
-          <h3 className="h3-bold text-center sm:text-left">My Tickets</h3>
-
-          <Button asChild size="lg" className="button hidden sm:flex">
-            <Link href="/#events">Explore More Events</Link>
-          </Button>
-        </div>
-      </section>
-
-      {/* <section className="wrapper my-8">
-        <Collection
-          data={events?.data}
-          emptyTitle="No Event Tickets Purchased Yet"
-          emptyStateSubtext="No worries - plenty of exciting events to explore"
-          collectionType="My_Tickets"
-          limit={2}
-          page={1}
-          urlParamName="ordersPage"
-          totalPages={2}
-        />
-      </section> */}
-
-      {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
         <div className="wrapper flex items-center justify-center sm:justify-between">
           <h3 className="h3-bold text-center sm:text-left">Events Organized</h3>
